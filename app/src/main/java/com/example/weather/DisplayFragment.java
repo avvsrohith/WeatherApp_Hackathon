86feb090f5cc6b5e7793006ebfd6b8b53f896c11
avvsrohith <avvsrohith@gmail.com>
2021-08-28 17:28:46 +0530

    updated

--- app/src/main/java/com/example/weather/DisplayFragment.java
+++ app/src/main/java/com/example/weather/DisplayFragment.java
@@ -2,16 +2,21 @@ package com.example.weather;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class DisplayFragment extends Fragment {
 
-    TextView condition;
+    TextView condition,windtv,humiditytv,pressuretv, temptv,minmaxtv,loctv,coortv;
+    public static ConstraintLayout c1,c2,c3,frameLayout;
+
+
 
 
     public DisplayFragment() {
@@ -32,10 +37,29 @@ public class DisplayFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.fragment_display, container, false);
         condition=v.findViewById(R.id.condition);
+        windtv=v.findViewById(R.id.windtv);
+        humiditytv=v.findViewById(R.id.humidityTv);
+        pressuretv=v.findViewById(R.id.pressuretv);
+        temptv =v.findViewById(R.id.temptv);
+        minmaxtv=v.findViewById(R.id.minmaxTv);
+        loctv=v.findViewById(R.id.loctv);
+        coortv=v.findViewById(R.id.coortv);
+        c1=v.findViewById(R.id.card1);
+        c2=v.findViewById(R.id.card2);
+        c3=v.findViewById(R.id.card3);
+        frameLayout=v.findViewById(R.id.frameLayout);
         return v;
     }
 
-    void setData(String cond){
+    void setData(String cond,float wind,float humidity,float pressure,float temp,float tempmax,float tempmin,String loc,float longitude,float latitude){
         condition.setText(cond);
+        windtv.setText(String.valueOf(wind)+" km/h");
+        humiditytv.setText(String.valueOf(humidity+" %"));
+        pressuretv.setText(String.valueOf(pressure)+" hpa");
+        temptv.setText(String.valueOf(temp)+" C");
+        minmaxtv.setText(String.valueOf(tempmin)+"C/"+String.valueOf(tempmax));
+        loctv.setText(loc);
+        coortv.setText(String.valueOf(longitude)+"/"+String.valueOf(latitude));
+
     }
 }
--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,28 +1,40 @@
 package com.example.weather;
 
 import static com.example.weather.API.API_utilities.API_KEY;
+import static com.example.weather.DisplayFragment.c1;
+import static com.example.weather.DisplayFragment.c2;
+import static com.example.weather.DisplayFragment.c3;
+import static com.example.weather.DisplayFragment.frameLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ActivityChooserView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentContainerView;
 import androidx.fragment.app.FragmentManager;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.weather.API.API_utilities;
 import com.example.weather.Model.Weather;
 import com.example.weather.Model.WeatherInfo;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.IOException;
 import java.util.List;
@@ -41,15 +53,17 @@ public class MainActivity extends AppCompatActivity {
     String cityname="Not found";
     FragmentContainerView fragmentContainerView;
     TextView failtv;
+    TextToSpeech textToSpeech;
+    WeatherInfo info;
+
+    Boolean dark=true;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
-        failtv=findViewById(R.id.Failtv);
         locationManager=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
@@ -60,11 +74,38 @@ public class MainActivity extends AppCompatActivity {
         Location location=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         cityname=getCity(location.getLongitude(),location.getLatitude());
 
+        textToSpeech=new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status==TextToSpeech.SUCCESS){
+                    int result=textToSpeech.setLanguage(Locale.getDefault());
+
+                }
+            }
+        });
+
+        bottomNavigationView=findViewById(R.id.bottomtabbar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.Mode:
+                        darkmode();
+                        return true;
+                    case R.id.tts:
+                            textToSpeech.speak(speak(),TextToSpeech.QUEUE_FLUSH,null);
+                        return true;
+                    case R.id.home:
+
+                        return true;
+                }
+                return false;
+            }
+        });
+
 
 
         FragmentManager fragmentManager=getSupportFragmentManager();
-
-
         fragmentContainerView=findViewById(R.id.fragmentContainerView);
         displayFragment=(DisplayFragment) fragmentManager.findFragmentByTag(DISPLAY_TAG);
         if(displayFragment==null){
@@ -72,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
             fragmentManager.beginTransaction().add(R.id.fragmentContainerView,displayFragment,DISPLAY_TAG).commit();
         }
 
-        getData(cityname);
+        getData("Visakhapatnam");
 
     }
 
@@ -86,9 +127,6 @@ public class MainActivity extends AppCompatActivity {
                     String city=address.getLocality();
                     if(city!=null && !city.equals("")){
                         cityname=city;
-                    }
-                    else{
-                        Toast.makeText(this, "City not found", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
@@ -105,11 +143,9 @@ public class MainActivity extends AppCompatActivity {
                     public void onResponse(Call<WeatherInfo> call, Response<WeatherInfo> response) {
                         if (response.body() == null) {
                             fragmentContainerView.setVisibility(View.INVISIBLE);
-                            failtv.setVisibility(View.VISIBLE);
                         } else{
-                            failtv.setVisibility(View.INVISIBLE);
-                            WeatherInfo info = response.body();
-                        displayFragment.setData(info.getName());
+                             info= response.body();
+                        displayFragment.setData(info.getWeather().get(0).getmain(),info.getWind().getSpeed(),info.getMain().getHumidity(),info.getMain().getPressure(),info.getMain().getTemp(),info.getMain().getTemp_max(),info.getMain().getTemp_min(),info.getName(),info.getCoord().getLon(),info.getCoord().getLat());
                     }
                     }
 
@@ -118,5 +154,44 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
+    }
+
+    public String speak(){
+        if(info==null){
+            return "No data";
+        }
+        else{
+            return "Location"+"\n"+info.getName()+"\n"+"weather"+"\n"+info.getWeather().get(0).getmain()+"\n"+"Wind speed"+"\n"+info.getWind().getSpeed()+"\n"+"Humidity"+"\n"+info.getMain().getHumidity()+"\n"
+                    +"Pressure"+"\n"+info.getMain().getPressure()+"\n"+"Temperature"+"\n"+info.getMain().getTemp()+"\n"+"Minimum temperature"+"\n"+info.getMain().getTemp_min()+"\n"+"Maximum temperature"+"\n"+info.getMain().getTemp_max()+"\n"+"Longitude"+"\n"+info.getCoord().getLon()+"\n"+"Latitude"+"\n"+info.getCoord().getLat();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if(textToSpeech!=null){
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    void darkmode(){
+        if(dark){
+            c1.setBackgroundResource(R.drawable.darkcardbg);
+            c2.setBackgroundResource(R.drawable.darkcardbg);
+            c3.setBackgroundResource(R.drawable.darkcardbg);
+            frameLayout.setBackgroundColor(Color.parseColor("#202125"));
+            bottomNavigationView.setItemBackgroundResource(R.drawable.grey_bg);
+            dark=false;
+        }
+        else
+        {
+            c1.setBackgroundResource(R.drawable.lightcardbg);
+            c2.setBackgroundResource(R.drawable.lightcardbg);
+            c3.setBackgroundResource(R.drawable.lightcardbg);
+            frameLayout.setBackgroundColor(Color.parseColor("#6782b4"));
+            bottomNavigationView.setItemBackgroundResource(R.drawable.lightbtmbg);
+            dark=true;
+        }
     }
 }
